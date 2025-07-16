21e43d79e05837e61eec51182a23fe804754c3d1
f4cmpro <f4cmpro@gmail.com>
2025-07-16 15:24:50 +0700

    problem 238

--- app/src/main/java/com/example/leetcode75/Solution.kt
+++ app/src/main/java/com/example/leetcode75/Solution.kt
@@ -1,10 +1,18 @@
 package com.example.leetcode75
 
 fun main() {
-    val problem151 = Problem151()
-    println("before:    Doan,   Le$*#* Tuan  ,,,, Tu   !_)@#,, A}|P{P{ ")
+    val problem238 = Problem238()
+    val nums = intArrayOf(1, 2, 3, 5, 8, 14, 15, 16, 18, 19, 23, 29, 33, 36, 41, 42, 43, 44, 53, 59, 60, 61, 62, 64, 67, 76, 87, 92, 93, 97)
     val start = System.currentTimeMillis()
-    println("reversed: ${problem151.reverseWords("   Doan,   Le\$*#* Tuan  ,,,, Tu   !_)@#,, A}|P{P{ ")}")
+    print("answers: [")
+    val answers = problem238.productExceptSelf(nums)
+    answers.forEachIndexed { i, ans ->
+        if (i == answers.lastIndex) {
+            println("$ans]")
+        } else {
+            print("$ans, ")
+        }
+    }
     val end = System.currentTimeMillis()
     println("Duration: ${end - start}ms")
 }
