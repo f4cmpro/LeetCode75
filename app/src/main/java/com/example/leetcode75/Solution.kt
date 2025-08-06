38898963a6f658a901ce7aa7ecfc6a5453da8fb3
f4cmpro <f4cmpro@gmail.com>
2025-08-06 20:18:14 +0700

    problem 1732

--- app/src/main/java/com/example/leetcode75/Solution.kt
+++ app/src/main/java/com/example/leetcode75/Solution.kt
@@ -1,10 +1,10 @@
 package com.example.leetcode75
 
 fun main() {
-    val problem = Problem1493()
-    val nums = intArrayOf(0,0,0,0)
+    val problem = Problem1732()
+    val nums = intArrayOf(-4,-3,-2,-1,4,3,2)
     val start = System.currentTimeMillis()
-    println("Longest Subarray of 1's After Deleting One Element: ${problem.longestSubarray(nums)}")
+    println("Find the Highest Altitude: ${problem.largestAltitude(nums)}")
     val end = System.currentTimeMillis()
     println("Duration: ${end - start}ms")
 }
